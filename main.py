ed7043d0f5dda2c67f7dcb31306eb44f976afbdb
AmbiNtsoah <ambinints04@gmail.com>
2025-11-27 06:25:03 +0400

    feat: add .gitignore file and setup first GUI for the app

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 """ Simple debut Main """
-greet = "Hello Python!"
+from PyQt5.QtWidgets import QApplication, QWidget
+import sys
+
+def main():
+    app = QApplication(sys.argv)
+    widget = QWidget()
+
+    widget.show()
+
+    app.exec_()
+    greet = "Hello Python!"
+    print(greet)
 
 if __name__ == "__main__":
-    print(greet)
+    main()
